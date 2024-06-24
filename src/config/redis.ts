c101684a4ebaabcf7a3003a637ab03293928a2bf
fasihh <fhk.tcs@gmail.com>
2024-06-24 18:15:28 +0500

    Using ioredis instead

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,5 @@
-import { createClient } from 'redis';
+import Redis from 'ioredis';
 
-const client = createClient();
+const redis: Redis = new Redis();
 
-export default client;
+export default redis;
