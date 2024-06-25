96a59e3fc2594f99ca348f702a2d24a5b4cb0eef
fasihh <fhk.tcs@gmail.com>
2024-06-25 16:32:48 +0500

    Made error handler handle any error instead of only request errors

--- src/api/utils/error_handler.ts
+++ src/api/utils/error_handler.ts
@@ -1,13 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
 import RequestError from '../exceptions/request_error';
 
-const errorHandler = (error: RequestError, req: Request, res: Response, next: NextFunction) => {
-    console.error(error);
-    res.status(error.status || 500).json({
+const errorHandler = (error: Error | RequestError, req: Request, res: Response, next: NextFunction) => {
+    let error_status: number;
+    let error_name: string;
+    let error_message: string;
+    let error_info: Record<string, string> | undefined;
+
+    if (error instanceof RequestError) {
+        error_status = error.status || 500;
+        error_name = error.name;
+        error_message = error.message;
+        error_info = error.info;
+    } else {
+        error_status = 500;
+        error_name = 'Server Error';
+        error_message = 'Internal Server Error';
+        error_info = undefined;
+    }
+
+    res.status(error_status).json({
         error: {
-            name: error.name || 'Server Error',
-            message: error.message || 'Internal Server Error',
-            info: error.info
+            name: error_name,
+            message: error_message,
+            info: error_info
         }
     });
 }
