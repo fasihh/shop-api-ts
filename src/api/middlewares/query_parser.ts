0e5a1d86443ab0997b00127a8863a7be33d6c18f
fasihh <fhk.tcs@gmail.com>
2024-06-26 12:15:33 +0500

    Added quantity parsing as well

--- src/api/middlewares/query_parser.ts
+++ src/api/middlewares/query_parser.ts
@@ -1,12 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 
 const queryParser = (req: Request, res: Response, next: NextFunction) => {
-    const { limit, offset }: { limit?: string, offset?: string } = req.query;
+    const {
+        search,
+        limit,
+        offset,
+        quantity
+    }: {
+        search?: string,
+        limit?: string,
+        offset?: string,
+        quantity?: string
+    } = req.query;
     
     req.queryParams = {
+        search,
         limit: parseInt(limit ?? '20', 10) || 20,
-        offset: parseInt(offset ?? '0', 10) || 0
-    }
+        offset: parseInt(offset ?? '0', 10) || 0,
+        quantity: parseInt(quantity ?? '1', 10) || 1 
+    };
 
     next();
 }
