8544b1e6c275896c0ccbec54ab149812724373d7
fasihh <fhk.tcs@gmail.com>
2024-06-26 12:15:45 +0500

    Added cart exceptions

--- src/api/exceptions/exceptions.ts
+++ src/api/exceptions/exceptions.ts
@@ -1,20 +1,24 @@
 export enum ExceptionType {
     INTERNAL_ERROR = 'INTERNAL_ERROR',
     USERNAME_CONFLICT = 'USERNAME_CONFLICT',
+    CART_CONFLICT = 'CART_CONFLICT',
     USER_NOT_FOUND = 'USER_NOT_FOUND',
     ITEM_NOT_FOUND = 'ITEM_NOT_FOUND',
+    CART_NOT_FOUND = 'CART_NOT_FOUND',
     INVALID_ID = 'INVALID_ID',
     INVALID_REQUEST = 'INVALID_REQUEST',
     AUTH_FAILURE = 'AUTH_FAILURE',
     INVALID_TOKEN = 'INVALID_TOKEN',
-    UNAUTHORIZED = 'UNAUTHORIZED'
+    UNAUTHORIZED = 'UNAUTHORIZED',
 }
 
 export const Exceptions: Record<ExceptionType, { status: number, message: string }> = {
     [ExceptionType.INTERNAL_ERROR]: { status: 500, message: 'An unexpected error occurred' },
     [ExceptionType.USERNAME_CONFLICT]: { status: 409, message: 'User with this name already exists' },
+    [ExceptionType.CART_CONFLICT]: { status: 409, message: 'User already has a cart that has not been checked out' },
     [ExceptionType.USER_NOT_FOUND]: { status: 404, message: 'User does not exist' },
     [ExceptionType.ITEM_NOT_FOUND]: { status: 404, message: 'Item does not exist' },
+    [ExceptionType.CART_NOT_FOUND]: { status: 404, message: 'Cart does not exist' },
     [ExceptionType.INVALID_ID]: { status: 400, message: 'User ID is invalid' },
     [ExceptionType.INVALID_REQUEST]: { status: 400, message: 'Invalid request' },
     [ExceptionType.AUTH_FAILURE]: { status: 401, message: 'Auth failure' },
