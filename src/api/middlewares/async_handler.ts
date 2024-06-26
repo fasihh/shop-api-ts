cd863c2c029c12dbaede0c4cbf991dfb3c8f931f
fasihh <fhk.tcs@gmail.com>
2024-06-26 13:23:08 +0500

    No longer logging error

--- src/api/middlewares/async_handler.ts
+++ src/api/middlewares/async_handler.ts
@@ -1,9 +1,6 @@
 import { NextFunction, Request, Response, RequestHandler } from "express";
 
 const asyncHandler = (callback: RequestHandler) => (req: Request, res: Response, next: NextFunction): Promise<void> => 
-    Promise.resolve(callback(req, res, next)).catch((error: unknown) => {
-        console.error(error);
-        next(error);
-    });
+    Promise.resolve(callback(req, res, next)).catch((error: unknown) => next(error));
 
 export default asyncHandler;
