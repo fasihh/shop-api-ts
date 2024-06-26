18ec17a0ba0b4e2bf1bf05a01f8603cbc2738fd3
fasihh <fhk.tcs@gmail.com>
2024-06-26 12:14:00 +0500

    CartInfo type added quantity attribute in RequestQuery added

--- src/api/types/global.d.ts
+++ src/api/types/global.d.ts
@@ -2,8 +2,11 @@ import type { Response } from 'express';
 
 export type ReturnResponse = Response<any, Record<string, any>>;
 
+export type CartInfo = { cart: Cart, cart_items: CartItem[] };
+
 export interface RequestQuery {
     search?: string;
     limit?: number;
     offset?: number;
+    quantity?: number;
 }
