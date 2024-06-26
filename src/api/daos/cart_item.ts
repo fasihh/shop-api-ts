16b26592f4309f870235b8cba28f221300f5f4b6
fasihh <fhk.tcs@gmail.com>
2024-06-26 12:16:54 +0500

    CartItemDAO added

---
+++ src/api/daos/cart_item.ts
@@ -0,0 +1,44 @@
+import CartItem from "../models/cart_item";
+import Item from "../models/item";
+
+class CartItemDAO {
+    async getByCartId(cart_id: number): Promise<CartItem[]> {
+        const cart_items: CartItem[] = await CartItem.findAll({
+            where: {
+                cart_id
+            },
+            include: [{
+                model: Item,
+                as: 'item'
+            }]
+        });
+
+        return cart_items;
+    }
+
+    async updateQuantity(id: number, quantity: number): Promise<number> {
+        const [count]: [affectedCount: number] = await CartItem.update({
+            quantity
+        }, { where: { id } });
+        return count;
+    }
+
+    async delete(id: number): Promise<number> {
+        const count: number = await CartItem.destroy({ where: { id } });
+        return count;
+    }
+
+    async create(item_id: number, cart_id: number, quantity: number = 1): Promise<CartItem> {
+        const cartItem = await CartItem.create({
+            item_id,
+            cart_id,
+            quantity
+        });
+
+        return cartItem;
+    }
+}
+
+const CartItemDAOInst = new CartItemDAO;
+
+export default CartItemDAOInst;
